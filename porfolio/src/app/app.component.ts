2f0262c0a37593325fbeac731eef3e3d746dc4ea
unknown <nlmnever2@gmail.com>
2022-07-20 14:06:08 +0630

    from PC

--- porfolio/src/app/app.component.ts
+++ porfolio/src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'porfolio';
   fakearray = new Array(100);
+
+  constructor() {
+    window.addEventListener('scroll', () => {
+      console.log(window.pageYOffset)
+      const currentScroll = window.pageYOffset;
+      const nav_div = document.getElementById('nav_div')
+
+      if  (currentScroll <= 0){
+        nav_div?.classList.add('top')
+      }
+    })
+  }
+  
 }
